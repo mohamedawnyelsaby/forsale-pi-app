28bd1a1878e07dd5499e15c4e139decc1cfede94
mohamedawnyelsaby <55304205+mohamedawnyelsaby@users.noreply.github.com>
2025-11-09 03:24:08 +0200

    Update theme-provider.tsx

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,11 +1,17 @@
-'use client'
+"use client"
 
-import * as React from 'react'
-import {
-  ThemeProvider as NextThemesProvider,
-  type ThemeProviderProps,
-} from 'next-themes'
+import * as React from "react"
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { type ThemeProviderProps } from "next-themes/dist/types"
+
+// 💥 تم التعديل لمعالجة الخطأ السابق
+// هذا يحل مشكلة Cannot find module '@/lib/utils'
+import { cn } from '../lib/utils' 
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  // ملاحظة: cn هنا يُستخدم عادةً لدمج Tailwind CSS Classes
+  // لكننا نستخدمه داخل <NextThemesProvider> لضمان تطبيق الثيم
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
+
+// يمكن إضافة أي مكونات أخرى تتعلق بالثيم (Theme) هنا
