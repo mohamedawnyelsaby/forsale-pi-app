b4b0c65a04f172795f8cae724e390a3c20fb2fc3
Mohamed awny <55304205+mohamedawnyelsaby@users.noreply.github.com>
2025-11-10 03:57:48 +0200

    Delete app/page.tsx

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-// app/page.tsx - الكود المؤقت للتشخيص
-export default function TemporaryTestPage() {
-  return (
-    <div style={{ padding: '50px', textAlign: 'center' }}>
-      <h1>If you see this, the routing works!</h1>
-    </div>
-  );
-}
